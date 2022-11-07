836fe0c446a5ebc630ec0e0a823986970a5bae7c
Guilherme Gomes do Braz <ggbraz@gmail.com>
2022-11-07 19:31:47 -0300

    implementando repositorio base

---
+++ CleanArchitecture.Data/Persistence/StreamerDbContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public static class StreamerDbContextSeed
+    {
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContext> logger)
+        {
+            if (!context.Streamers.Any())
+            {
+                context.Streamers.AddRange(GetPreconfiguredStreamer());
+                await context.SaveChangesAsync();
+                logger.LogInformation("Inserted news records in db {context}", typeof(StreamerDbContext).Name);
+            }
+        }
+
+        private static IEnumerable<Streamer> GetPreconfiguredStreamer()
+        {
+            return new List<Streamer>
+            {
+                new Streamer {CreatedBy= "zarbg", Name="Maxi HBP", Url="http://www.hbp.com" },
+                new Streamer {CreatedBy= "zarbg", Name="Amazon VIP", Url="http://www.amazonvip.com" },
+            };
+        }
+    }
+}
---
+++ CleanArchitecture.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,73 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : BaseDomainModel
+    {
+        protected readonly StreamerDbContext _context;
+
+        public RepositoryBase(StreamerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeString = null,
+            bool disableTracking = true)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+            if(string.IsNullOrWhiteSpace(includeString)) query = query.Include(includeString);
+            if(predicate != null) query = query.Where(predicate);
+            if(orderBy!=null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<Expression<Func<T, object>>> includes = null,
+            bool disableTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
